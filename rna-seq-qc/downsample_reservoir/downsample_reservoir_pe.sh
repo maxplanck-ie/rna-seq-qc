bbea93e32711e625dc64e93e2202857046c6b028
Fabian Kilpert <kilpert@ie-freiburg.mpg.de>
2015-08-04 17:18:08 +0200

    Default version of reservoir wrapper scripts using zcat and gzip

--- rna-seq-qc/downsample_reservoir/downsample_reservoir_pe.sh
+++ rna-seq-qc/downsample_reservoir/downsample_reservoir_pe.sh
@@ -5,13 +5,13 @@
 
 bindir=$(dirname $0)
 
-DIFF=$(diff <(pigz -dc ${2} | sed -n '1p;1~4p' | cut -d " " -f1) <(pigz -dc ${3} | sed -n '1p;1~4p' | cut -d " " -f1))
+DIFF=$(diff <(zcat ${2} | sed -n '1p;1~4p' | cut -d " " -f1) <(zcat ${3} | sed -n '1p;1~4p' | cut -d " " -f1))
 if [ "$DIFF" != "" ] 
 then
     echo "Error! FASTQ sequence identifiers do NOT match!"
     exit 1
 else
-    paste <(pigz -dc ${2} | sed '/^$/d' | paste -d "\t" - - - - ) <(pigz -dc ${3} | sed '/^$/d' | paste -d "\t" - - - - ) \
+    paste <(zcat ${2} | sed '/^$/d' | paste -d "\t" - - - - ) <(zcat ${3} | sed '/^$/d' | paste -d "\t" - - - - ) \
     | $bindir/reservoir ${1} \
-    | tee >(cut -f1,2,3,4 | sed 's/\t/\n/g' | pigz -9 > ${4}) >(cut -f5,6,7,8 | sed 's/\t/\n/g' | pigz -9 > ${5}) >/dev/null
+    | tee >(cut -f1,2,3,4 | sed 's/\t/\n/g' | gzip > ${4}) >(cut -f5,6,7,8 | sed 's/\t/\n/g' | gzip > ${5}) >/dev/null
 fi
--- rna-seq-qc/downsample_reservoir/downsample_reservoir_se.sh
+++ rna-seq-qc/downsample_reservoir/downsample_reservoir_se.sh
@@ -5,4 +5,4 @@
 
 bindir=$(dirname $0)
 
-pigz -dc ${2} | sed '/^$/d' | paste -d "\t" - - - - | $bindir/reservoir ${1} | sed 's/\t/\n/g' | pigz -9 > ${3}
+zcat ${2} | sed '/^$/d' | paste -d "\t" - - - - | $bindir/reservoir ${1} | sed 's/\t/\n/g' | gzip > ${3}
