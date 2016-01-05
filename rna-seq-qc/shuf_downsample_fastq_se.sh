09b513582f3d3e2ce6c64b7aa8782e0e0158366f
Fabian Kilpert <kilpert@ie-freiburg.mpg.de>
2016-01-05 11:19:54 +0100

    updated file structure (some deleted)

--- rna-seq-qc/shuf_downsample_fastq_se.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-## Usage: bash shuf_downsample_fastq_se.sh 1000 a.fastq.gz a.downsampled.fastq.gz
-## Downsampling has NO seed!!!
-
-zcat ${2} | sed '/^$/d' | paste -d "\t" - - - - | shuf -n ${1} | sed 's/\t/\n/g' | gzip > ${3}
