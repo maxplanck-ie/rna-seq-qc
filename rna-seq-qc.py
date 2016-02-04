9c268f717b59d6bb4c5f63db9a6b55384fa1bdc1
fkilpert <fkilpert@gmail.com>
2016-02-04 15:44:47 +0100

    adjusted width of help text

--- rna-seq-qc.py
+++ rna-seq-qc.py
@@ -7,7 +7,8 @@ __description__ = """
     {version}
     =================
 
-    RNA-seq pipeline for processing RNA sequence data from high throughput sequencing.
+    RNA-seq pipeline for processing RNA sequence data from high throughput
+    sequencing.
 
     Fabian Kilpert - February 04, 2016
     email: kilpert@ie-freiburg.mpg.de
@@ -15,24 +16,27 @@ __description__ = """
     This software is distributed WITHOUT ANY WARRANTY!
     --------------------------------------------------
 
-    Following steps are executed in succession: FASTQ subsampling (optional), quality check
-    with FASTQC, trimming of reads with Trim Galore (optional), estimation of insert size
-    and strand specificity with RSeQC, mapping with TopHat, extensive quality check with RSeQC,
-    counting of features with featureCounts (default) or htseq-count, differential expression
-    analysis with DESeq2 (optional).
+    Following steps are executed in succession: FASTQ subsampling (optional),
+    quality check with FASTQC, trimming of reads with Trim Galore (optional),
+    estimation of insert size and strand specificity with RSeQC, mapping with
+    TopHat, extensive quality check with RSeQC, counting of features with
+    featureCounts (default) or htseq-count, differential expression analysis
+    with DESeq2 (optional).
 
-    The pipeline requires gzipped FASTQ files (.fastq.gz) for input, which are loaded
-    from an input directory (-i INDIR). Read files belonging together require the exact same
-    base name but ending either in "_R1" or "_R2" right in front of the .fastq.gz extension.
-    (e.g. reads_R1.fastq.gz, reads_R2.fastq.gz). In addition, a specific genome version argument
-    must be provided (e.g. -g mm10) to define the reference data used for annotation.
-    This loads a number of indexes for mapping programs (Bowtie2, TopHat2, HISAT2 (new!), etc.) from the
-    corresponding configuration file of the rna-seq-qc sub-folder (e.g. rna-seq-qc/mm10.cfg).
-    Additional genomes for selection can be provided as cfg-files by the user. The pipeline
-    works for single end and paired end sequences alike.
+    The pipeline requires gzipped FASTQ files (.fastq.gz) for input, which are
+    loaded from an input directory (-i INDIR). Read files belonging together
+    require the exact same base name but ending either in "_R1" or "_R2" right
+    in front of the .fastq.gz extension (e.g. reads_R1.fastq.gz,
+    reads_R2.fastq.gz). In addition, a specific genome version argument must be
+    provided (e.g. -g mm10) to define the reference data used for annotation.
+    This loads a number of indexes for mapping programs (Bowtie2, TopHat2,
+    HISAT2 (new!), etc.) from the corresponding configuration file of the
+    rna-seq-qc sub-folder (e.g. rna-seq-qc/mm10.cfg). Additional genomes for
+    selection can be provided as cfg-files by the user. The pipeline works for
+    single end and paired end sequences alike.
 
-    The DE analysis is only executed if a valid setup table is provided
-    (e.g. --DE setup_table.tsv), which defines the relationships of the samples.
+    The DE analysis is only executed if a valid setup table is provided (e.g.
+    --DE setup_table.tsv), which defines the relationships of the samples.
 
     More information on the pipeline can be found on the wiki page:
     http://epicenter/wiki/index.php/RNA-seq_pipeline_(rna-seq-pc.py)
