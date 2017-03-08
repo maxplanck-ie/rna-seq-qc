edbcdca08e42d5eb8f3038697f79018e093049fc
fkilpert <fkilpert@gmail.com>
2017-03-08 13:36:45 +0100

    updated report table

--- rna-seq-qc/Report_table.R
+++ rna-seq-qc/Report_table.R
@@ -34,12 +34,12 @@ print("Generating project report...")
 
 unpair <- function(allfiles){
   files = c()
-  names = c()
+  samples = c()
   for (file in allfiles) {
-    name = gsub("_R1.fastq.gz$|_R2.fastq.gz$","",basename(file))
+    sample = gsub("_R1.fastq.gz$|_R2.fastq.gz$","",basename(file))
     
-    if (!name %in% names) {
-      names = c(names, name)
+    if (!sample %in% samples) {
+      samples = c(samples, sample)
       files = c(files, file)
     } 
   }  
@@ -49,20 +49,19 @@ unpair <- function(allfiles){
 
 ## check if reads are paired ###################################################
 is_paired = function(allfiles){
-  names = c()
+  samples = c()
   paired = F
   for (file in allfiles) {
-    name = gsub("_R1.fastq.gz$|_R2.fastq.gz$","",basename(file))
+    sample = gsub("_R1.fastq.gz$|_R2.fastq.gz$","",basename(file))
     
-    if (!name %in% names) {
-      names = c(names, name)
+    if (!sample %in% samples) {
+      samples = c(samples, sample)
     } else {
       paired = T
     }
   }  
   return(paired)
 }
-
 
 
 ## FASTQ: total ################################################################
@@ -80,18 +79,18 @@ if (file.exists(indir)){
   
   num_reads = c()
   files = c()
-  names = c()
+  samples = c()
   
   for (file in unpair(allfiles)) {
     
-    if (paired==T) { name = gsub("_R1.fastq.gz$|_R2.fastq.gz$","",basename(file)) 
-    } else { name = gsub(".fastq.gz$","",basename(file)) }
-    names = c(names, name)
+    if (paired==T) { sample = gsub("_R1.fastq.gz$|_R2.fastq.gz$","",basename(file)) 
+    } else { sample = gsub(".fastq.gz$","",basename(file)) }
+    samples = c(samples, sample)
     num = as.numeric( system(sprintf("zcat %s | wc -l | awk '{print $1/4}'", file), intern=T) )
     num_reads = c( num_reads, num )  
-    cat(name, num, "\n")
+    cat(sample, num, "\n")
   }
-  report = data.frame(row.names=names, FASTQ=num_reads)
+  report = data.frame(samples=samples, FASTQ=num_reads)
   report$FASTQ_perc = 100.0
 } else {
   cat( paste("Error! Directory does NOT exist:\n",indir) )
@@ -107,23 +106,19 @@ indir = file.path(main_outdir,"Trim_Galore")
 if ( file.exists( indir ) ){
   files <- sort(list.files(indir, pattern="*.fastq.gz$", full.names=T))
   files
-  names = c()
+  samples = c()
   counts = c()
   for (file in unpair(files)) {
-    if (paired==T) { name = gsub("_R1.fastq.gz$|_R2.fastq.gz$","",basename(file)) 
-    } else { name = gsub(".fastq.gz$","",basename(file)) }
-    names = c( names, name )
+    if (paired==T) { sample = gsub("_R1.fastq.gz$|_R2.fastq.gz$","",basename(file)) 
+    } else { sample = gsub(".fastq.gz$","",basename(file)) }
+    samples = c( samples, sample )
     count = as.numeric( system(sprintf("zcat %s | wc -l | awk '{print $1/4}'", file), intern=T) )
     counts = c( counts, count )
-    cat(paste(name, num, "\n"), sep=" ")
+    cat(paste(sample, count, "\n"), sep=" ")
   }
-  df_counts = data.frame(names=names, TrimGalore=counts)
-  
-  report = merge(report, df_counts, by.x=c("row.names"), by.y=("names"))
-  rownames(report) = report$Row.names
-  report$Row.names = NULL
-  
-  report$TrimGalore_perc = apply(report, 1, function(x) x["TrimGalore"]/x["FASTQ"]*100)
+  df_counts = data.frame(samples=samples, TrimGalore=counts)
+  report = merge(report, df_counts, by="samples")
+  report$TrimGalore_perc = report$TrimGalore / report$FASTQ * 100
   report
 } 
 
@@ -136,25 +131,21 @@ if ( file.exists( indir ) ){
   ## files = sample(files) ## debugging only!!!
   files
   counts = c()
-  names = c()
+  samples = c()
   
   for (file in files) {
-    name = gsub(".bam$","",basename(file))
-    names = c(names, name)
+    sample = gsub(".bam$","",basename(file))
+    samples = c(samples, sample)
 
     ## count mapped (-F4) and primary (-F256) fragments
     count = as.numeric( system(sprintf("%s view -c -F260 %s | awk '{print $1/2}'", file.path(samtools_dir,"samtools"), file), intern=T) )
 
     counts = c( counts, count )
-    cat(paste(name, num, "\n"), sep=" ")
+    cat(paste(sample, count, "\n"), sep=" ")
   }
-  df_counts = data.frame(names=names, HISAT2=counts)
-  
-  report = merge(report, df_counts, by.x=c("row.names"), by.y=("names"))
-  rownames(report) = report$Row.names
-  report$Row.names = NULL
-  
-  report$HISAT2_perc = apply(report, 1, function(x) x["HISAT2"]/x["FASTQ"]*100)
+  df_counts = data.frame(samples=samples, HISAT2=counts)
+  report = merge(report, df_counts, by="samples")
+  report$HISAT2_perc = report$HISAT2 / report$FASTQ * 100
   report
 }
 
@@ -166,25 +157,21 @@ if ( file.exists( indir ) ){
   ## files = sample(files) ## debugging only!!!
   files
   counts = c()
-  names = c()
+  samples = c()
   
   for (file in files) {
-    name = gsub(".bam$","",basename(file))
-    names = c(names, name)
+    sample = gsub(".bam$","",basename(file))
+    samples = c(samples, sample)
     
     ## count mapped (-F4) and primary (-F256) fragments
     count = as.numeric( system(sprintf("%s view -c -F260 %s | awk '{print $1/2}'", file.path(samtools_dir,"samtools"), file), intern=T) )
 
     counts = c( counts, count )
-    cat(paste(name, num, "\n"), sep=" ")
+    cat(paste(sample, count, "\n"), sep=" ")
   }
-  df_counts = data.frame(names=names, TopHat2=counts)
-  
-  report = merge(report, df_counts, by.x=c("row.names"), by.y=("names"))
-  rownames(report) = report$Row.names
-  report$Row.names = NULL
-  
-  report$TopHat2_perc = apply(report, 1, function(x) x["TopHat2"]/x["FASTQ"]*100)
+  df_counts = data.frame(samples=samples, TopHat2=counts)
+  report = merge(report, df_counts, by="samples")
+  report$TopHat2_perc = report$TopHat2 / report$FASTQ * 100
   report
 }
 
@@ -198,12 +185,9 @@ if ( file.exists( infile ) ) {
  counts = read.table(infile, header=TRUE)
  head(counts)
 
- df_counts = data.frame("featureCounts"=apply(counts, 2, sum))
- report = merge(report, df_counts, by.x=c("row.names"), by.y=("row.names"))
- row.names(report) = report$Row.names
- report$Row.names = NULL
- 
- report$featureCounts_perc = apply(report, 1, function(x) x["featureCounts"]/x["FASTQ"]*100)
+ df_counts = data.frame( row.names=NULL,samples=colnames(counts), featureCounts=apply(counts,2,sum) )
+ report = merge(report, df_counts, by="samples")
+ report$featureCounts_perc = report$featureCounts / report$FASTQ * 100
  report
 }
 
@@ -215,14 +199,12 @@ if ( file.exists( infile ) ) {
   counts = read.table(infile, header=TRUE)
   head(counts)
   
-  df_counts = data.frame( "htseq_count"=apply(counts, 2, sum) )
-  report = merge(report, df_counts, by.x=c("row.names"), by.y=("row.names"))
-  row.names(report) = report$Row.names
-  report$Row.names = NULL
-
-  report$htseq_counts_perc = apply(report, 1, function(x) x["htseq_count"]/x["FASTQ"]*100)
+  df_counts = data.frame( row.names=NULL, samples=colnames(counts), htseq_count=apply(counts,2,sum) )
+  report = merge(report, df_counts, by="samples")
+  report$htseq_count_perc = report$htseq_count / report$FASTQ * 100
   report
 }
 
+rownames(report) = report$samples
+report$samples = NULL
 write.table(report,"Report.tsv", sep="\t", quote=FALSE, col.names=NA) # save to file
-
